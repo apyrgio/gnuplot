525cf7b2a00390b80dfcc8aa3eaf4a3ca6d16969
Alex Pyrgiotis <apyrgio@grnet.gr>
2013-08-20 17:42:28 +0300

    Plot "true" comparison results effectively

--- comp-truth/comp-truth.sh
+++ comp-truth/comp-truth.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 export FORCE_WCP="writeback"
-#export FORCE_CACHE_OBJECTS="cs*8"
-export FORCE_CACHE_SIZE="bs/64 bs/32 bs/16 bs/8"
+export FORCE_CACHE_OBJECTS="cs*8"
+export FORCE_CACHE_SIZE="bs/8"
 export FORCE_IODEPTH="16"
 export FORCE_THREADS="multi"
 export FORCE_BENCH_OBJECTS="bounded"
@@ -10,21 +10,19 @@ export FORCE_BENCH_SIZE="2G"
 export FORCE_BLOCK_SIZE="4k"
 export FORCE_USE_CACHED="no"
 
-rm ~/sosd/*
-rm ~/cached/*
+rm ~/sosd3/*
+rm ~/cached3/*
 
-#for bs in 4k 8k 16k 32k 64k; do
-#	export FORCE_BLOCK_SIZE=$bs
-#	~/scripts/stress_cached.sh -l ~/log -r ~/sosd \
-#		-prog no -y -w -v 1
-#done
+~/scripts/stress_cached.sh -l ~/log3 -r ~/sosd3 \
+	-prog no -rtype io,lat -y -w -v 1
 
 export FORCE_USE_CACHED="yes"
+export FORCE_CACHE_SIZE="bs/64 bs/32 bs/16 bs/8"
 
 I=0
 for co in cs*1 cs*2 cs*4 cs*8; do
 	I=$(( $I+1 ))
 	export FORCE_CACHE_OBJECTS=$co
-	~/scripts/stress_cached.sh -l ~/log -r ~/cached \
-		-prog no -rtype io,lat -rpost $I -y -w -v 1
+	~/scripts/stress_cached.sh -l ~/log3 -r ~/cached3 \
+		-prog no -rtype io,lat -rpost $I -w -y -v 1
 done
